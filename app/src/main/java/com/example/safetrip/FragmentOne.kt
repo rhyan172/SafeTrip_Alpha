6664c43f509c691017f5f68379b73ab7fb72565e
DESKTOP-1KCUGGB\rhyan <michaelbugoy17@gmail.com>
2021-10-24 23:17:41 +0800

    latest update: the app is now working. sequence: splash screen > Intro Slider

--- app/src/main/java/com/example/safetrip/FragmentOne.kt
+++ app/src/main/java/com/example/safetrip/FragmentOne.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.safetrip.R
+
 class Intro1Fragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
--- app/src/main/java/com/example/safetrip/IndicatorLayout.kt
+++ app/src/main/java/com/example/safetrip/IndicatorLayout.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
+import com.example.safetrip.R
+
 class IndicatorLayout : LinearLayout {
     private var indicatorCount: Int = 0
     private var selectedPosition: Int = 0
