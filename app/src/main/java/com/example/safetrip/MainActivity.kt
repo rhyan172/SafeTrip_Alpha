a33d598ad8ae8d734b150a07d47565e8bb0edba3
DESKTOP-1KCUGGB\rhyan <michaelbugoy17@gmail.com>
2021-11-16 16:48:50 +0800

    updated. status: work in progress at login activity

--- app/src/main/java/com/example/safetrip/MainActivity.kt
+++ app/src/main/java/com/example/safetrip/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.safetrip
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val logout = findViewById<Button>(R.id.btnLogout)
+        logout.setOnClickListener {
+            val intent = Intent(this, LoginSignup::class.java)
+            Firebase.auth.signOut()
+            startActivity(intent)
+        }
     }
 }
